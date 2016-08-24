f83c8d452fe5e609a15aad00e9a3f7c8a22e152e
Alex Bryant <alex.bryant@neubus.com>
2016-08-24 10:07:00 -0500

    added RequestRedaction Driver
    added RequestRedaction Driver

--- TestDriverList.rb
+++ TestDriverList.rb
@@ -1,8 +1,10 @@
 require_relative 'testDrivers/TestDriver'
 require_relative 'testDrivers/OCMain'
 require_relative 'testDrivers/ViewExternalFile'
+require_relative 'testDrivers/RequestRedaction'
 
 $testDrivers = [
                 OCMain,
-                ViewExternalFile
+                ViewExternalFile,
+                RequestRedaction
                 ]
