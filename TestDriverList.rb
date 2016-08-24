acf85edaf7cfc3a98481250e6c54e9a2e1adf7d1
Alex Bryant <alex.bryant@neubus.com>
2016-08-24 11:06:09 -0500

    syntax fix for TestDriverList
    syntax fixes for TestDriverList

--- TestDriverList.rb
+++ TestDriverList.rb
@@ -2,11 +2,11 @@ require_relative 'testDrivers/TestDriver'
 require_relative 'testDrivers/OCMain'
 require_relative 'testDrivers/ViewExternalFile'
 require_relative 'testDrivers/RequestRedaction'
-require_relative 'testDrivers/requestStatus'
+require_relative 'testDrivers/RequestStatus'
 
 $testDrivers = [
                 OCMain,
                 ViewExternalFile,
-                RequestRedaction
+                RequestRedaction,
                 RequestStatus
                 ]
