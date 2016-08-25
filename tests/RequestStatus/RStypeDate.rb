18543b73facf84f1b26879a1665a9a276d874a1c
kevinnorwood <knorwood@neuone.com>
2016-08-25 14:21:19 -0500

    Updated RStypeDate

--- tests/RequestStatus/RStypeDate.rb
+++ tests/RequestStatus/RStypeDate.rb
@@ -2,32 +2,32 @@ class RStypeDate < Test
 
 
   def did_test_pass
+    didPass = true
     t = Time.now
     dateFields = [
       @driver.find_element(:id, "dateFrom"),
       @driver.find_element(:id, "dateTo")
     ]
     dateFields.each do |field|
+      field.click
       field.send_keys("#{t.strftime('%D')}")
       calendar = @driver.find_element(:id, "ui-datepicker-div")
       cal_month = @driver.find_element(:class, "ui-datepicker-month")
       cal_year = @driver.find_element(:class, "ui-datepicker-year")
-      # sleep 2
+      # puts calendar.displayed?
       if (calendar.displayed?) then
         if (cal_month.text == t.strftime("%B")) then #current month
           if (cal_year.text != t.strftime("%Y")) #current year
-            return false
-          else
-            return true
+            didPass = false
           end
         else
-          return false
+          didPass = false
         end
       else
-        return false
+        didPass = false
       end
     end
-    return false
+    return didPass
   rescue => e
     puts "An error has occurred in #{self.class.name}.#{__method__}"
     puts e.message
