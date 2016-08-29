c88525f0f5bf07a4a92540f3093a44c67bf10632
kevinnorwood <knorwood@neuone.com>
2016-08-29 11:20:49 -0500

    Fixed RResSubmitForm

--- tests/RequestResearch/RResSubmitForm.rb
+++ tests/RequestResearch/RResSubmitForm.rb
@@ -2,14 +2,17 @@ class RResSubmitForm < Test
 
 
   def did_test_pass
-    # wait = Selenium::WebDriver::Wait.new(:timeout => 5400, :interval => 1)
-    # wait.until { @driver.title = "View External File" }
-    # if ((@driver.find_element(:xpath, "//*[@id='requestRedactionResultDiv']/span/h3")).displayed?)
-    #   return true
-    # else
-    #   return false
-    # end
-    return false
+    wait = Selenium::WebDriver::Wait.new(:timeout => 5400, :interval => 1)
+    wait.until { @driver.title == "View External File" }
+    # puts @driver.current_url
+    if (@driver.current_url == "https://qa1.neuone.com/rhamilton/onecase4200/index.php/request/external#")
+      if (@driver.title == "View External File")
+        if ((@driver.find_element(:xpath, "//*[@id='requestRedactionResultDiv']/span/h3")).displayed?)
+          return true
+        end
+      end
+    end
+  return false
   rescue => e
     puts "An error has occurred in #{self.class.name}.#{__method__}"
     puts e.message
@@ -21,23 +24,22 @@ class RResSubmitForm < Test
     values = ['jsmith','John','Smith','21 Jump Street','Houston','77014']
     dropdown = ['State', 'request_type']
     dvalues = ['193', '550']
+    go = @driver.find_element(:id, "go_button")
     if (@driver.title.downcase.start_with? "request research")
       (0..5).each do |num|
-        puts num
         field = @driver.find_element(:id, "#{text_input[num]}")
-        puts values[num]
-        puts field.attribute("value")
-        field.send_keys [:control, 'a']
-        field.send_keys("#{values[num]}")
-      sleep 1
+        if (field.attribute("value") == "")
+          field.send_keys("#{values[num]}")
+        end
+      # puts field.attribute("value")
       end
       (0..1).each do |num|
         menu = @driver.find_element(:id, "#{dropdown[num]}")
         menu.find_element(:css, "option[value = '#{dvalues[num]}']").click
       end
     end
-    # go = @driver.find_element(:id, "go_button")
-    # go.click
+    go.click
+
   rescue => e
     puts "An error has occurred in #{self.class.name}.#{__method__}"
     puts e.message
